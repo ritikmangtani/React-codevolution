7bb25fe0c514757ac3fa392b249d293ec9ed6177
ritikmangtani <mangtaniritik@gmail.com>
2021-07-10 17:13:07 +0530

    upto css basics

---
+++ hello-world/src/Form.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Form extends Component {
+    render() {
+        return (
+            <div>
+                <label>username</label>
+                <input type='text' />                
+            </div>
+        )
+    }
+}
+
+export default Form
